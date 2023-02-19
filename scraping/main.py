152f43fd885c9e78dc3cf939b941179596a0d549
u8y7541 <surya.subbarao1@gmail.com>
2023-02-18 23:51:05 -0800

    Update main scraper and sample output

--- scraping/main.py
+++ scraping/main.py
@@ -7,11 +7,18 @@ from bs4 import BeautifulSoup
 from scrapers.residential import scrape_residential
 from scrapers.study import scrape_study
 from scrapers.rendezvous import scrape_rendezvous
+from scrapers.bcafe import scrape_bcafe
+from scrapers.drey import scrape_drey
+from scrapers.epic_ackerman import scrape_epic_ackerman
 
 data = [] 
 data.extend(scrape_residential(datetime.today().strftime('%Y-%m-%d')))
 data.extend(scrape_study())
 data.extend(scrape_rendezvous())
+data.extend(scrape_bcafe())
+data.extend(scrape_drey())
+data.extend(scrape_epic_ackerman())
+
 file = open("sample.json", "w")
 json.dump(data, file, indent=4)
 file.close()
