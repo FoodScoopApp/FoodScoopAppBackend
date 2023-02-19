523d9f1224ea0aab818c3bee2fd34e7fa9ad9cf2
u8y7541 <surya.subbarao1@gmail.com>
2023-02-18 23:50:33 -0800

    Finished The Drey and Epic at Ackerman
    Both use the same code; might extract this into its own method

---
+++ scraping/scrapers/drey.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from .utils import *
+
+def scrape_drey():
+    print("Scraping 'The Drey'...")
+    meal_list = []
+    drey_url = "http://menu.dining.ucla.edu/Menus/Drey"
+    drey_page = requests.get(drey_url).text
+    drey = BeautifulSoup(drey_page, PARSER)
+
+    for i in drey.find_all(class_="menu-item"):
+        meal_list.append(build_menu_item(i,"The Drey",["lunch","dinner"],""))
+
+    return meal_list
