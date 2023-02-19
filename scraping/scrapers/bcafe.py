79bf4c4fa2c6ae8a42c9d67d05bffc538c51826d
u8y7541 <surya.subbarao1@gmail.com>
2023-02-18 23:49:50 -0800

    Finished Bruin Café

---
+++ scraping/scrapers/bcafe.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+from .utils import *
+
+def scrape_bcafe():
+    print("Scraping 'Bruin Café'...")
+    meal_list = []
+    bcafe_url = "http://menu.dining.ucla.edu/Menus/BruinCafe"
+    bcafe_page = requests.get(bcafe_url).text
+    bcafe = BeautifulSoup(bcafe_page, PARSER)
+
+    for (day, dateObject) in weekdays.items():
+        div = bcafe.find("h4", string=day+':').parent;
+        for i in div.find_all(class_="menu-item"):
+            meal_list.append(build_menu_item(i,"Bruin Café",["lunch"],
+                                             dateObject.strftime('%Y-%m-%d')))
+
+    # TODO: fix entree soups special case
+    menu_blocks = bcafe.find_all("div", class_="menu-block")[2:]
+    for menu_block in menu_blocks:
+        for i in menu_block.find_all(class_="menu-item"):
+            meal_list.append(build_menu_item(i,"Bruin Café",["lunch","dinner"],""))
+        menu_block = menu_block.next_sibling
+
+    return meal_list
