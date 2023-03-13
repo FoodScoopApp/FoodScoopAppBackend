42f02fc78210cb4e0a536651a249e1cab14054f7
weitcis <weitcis@pm.me>
2023-03-13 04:07:18 -0700

    refactor: split notification generation func

--- notification.ts
+++ notification.ts
@@ -1,8 +1,8 @@
 import moment from "moment";
 import config from "./config";
 import { convertDiningHall, dateFormat, getCurrentMealPeriodForDiningHall, timeFormat } from "./FoodScoopAppTypes/converters";
-import { DiningHall as TypeDiningHall, DiningHallName, User as TypeUser } from "./FoodScoopAppTypes/models";
-import { DiningHall as SchemeDiningHall, Meal, User as SchemeUser } from "./models";
+import { DiningHall as TypeDiningHall, DiningHallName, MealPeriod, User as TypeUser } from "./FoodScoopAppTypes/models";
+import { DiningHall as SchemaDiningHall, User as SchemaUser } from "./models";
 import { Expo } from 'expo-server-sdk';
 
 
@@ -19,8 +19,8 @@ const thresholds: { [Property in DiningHallName]: number } = {
 }
 
 export async function sendNotifications() {
-	const users = await SchemeUser.find({});
-	const dhs = await SchemeDiningHall.find({ date: moment().format(dateFormat) })
+	const users = await SchemaUser.find({});
+	const dhs = await SchemaDiningHall.find({ date: moment().format(dateFormat) })
 	const dhTypeMap: { [Property in DiningHallName]: TypeDiningHall | null } = {
 		BP: null,
 		DN: null,
@@ -60,6 +60,75 @@ export async function sendNotifications() {
 
 type NotificationObject = { body: string, title: string }
 
+// INFO: This checking function is for activityLevel
+// notifications only, consider better naming
+function checkAlreadySent(dh: TypeDiningHall, user: TypeUser): boolean {
+	if (!user.notificationsSent) return false
+	const lastSentForDh = user.notificationsSent[dh.name]
+	const time = moment(lastSentForDh, timeFormat)
+	const mealPeriod = getCurrentMealPeriodForDiningHall(dh)
+	if (mealPeriod) {
+		const startTime = moment(mealPeriod.startTime, timeFormat)
+		if (time.diff(startTime) > 0) {
+			return true
+		}
+	}
+	return false
+}
+
+async function generateMealNotifications(
+	mealPeriod: MealPeriod,
+	favMeals: string[],
+	dhFullname: string): Promise<NotificationObject[]> {
+	let results: NotificationObject[] = []
+	const meals = mealPeriod.subcategories.flatMap((subcategory) => {
+		return subcategory.meals
+	})
+	// INFO: consider merging into one notification
+	// factors to consider: too many meals
+	// that cannot be shown in the title / body
+	for (const meal of meals) {
+		if (favMeals.includes(meal)) {
+			results.push({
+				title: `${dhFullname} is now serving ${meal} !`,
+				body: ``
+			})
+		}
+	}
+	return results
+}
+
+async function generateActivityNotification(dh: TypeDiningHall, user: TypeUser): Promise<NotificationObject[]> {
+	let results: NotificationObject[] = []
+	if (!dh.activityLevel) return []
+	if (checkAlreadySent(dh, user)) return []
+	if (dh.activityLevel <= thresholds[dh.name]) {
+		const dhFullname = convertDiningHall[dh.name]
+		results.push({ title: `${dhFullname} activity is low now!`, body: `` })
+		await updateUserRecord(user, dh.name)
+	}
+	return results
+}
+
+async function generateOpeningNotifications(mealPeriod: MealPeriod, user: TypeUser, dhFullname: string): Promise<NotificationObject[]> {
+	let results: NotificationObject[] = []
+	const startTime = moment(mealPeriod.startTime, timeFormat)
+	// INFO: consider finding a better way to determine if already sent
+	// one possible way to avoid missing / duplicated notifications
+	// is to call this larger intervals
+	if (Math.abs(moment().diff(startTime, 's')) < 60) {
+		// Opening notification
+		results.push({ title: `${dhFullname} is now open!`, body: `` })
+
+		// Notification for favorite meal
+		const favMeals = user.favMeals
+		if (favMeals) {
+			results = await generateMealNotifications(mealPeriod, favMeals, dhFullname)
+		}
+	}
+	return results
+}
+
 async function generateNotification(user: TypeUser,
 	dhs: { [Property in DiningHallName]: TypeDiningHall | null }): Promise<NotificationObject[]> {
 	let results: NotificationObject[] = []
@@ -67,59 +136,20 @@ async function generateNotification(user: TypeUser,
 	for (const dhName of user.favDiningHalls) {
 		const dh = dhs[dhName]
 		if (!dh) continue
-		if (user.notificationsSent) {
-			const lastSentForDh = user.notificationsSent[dh.name]
-			const time = moment(lastSentForDh, timeFormat)
-			const mealPeriod = getCurrentMealPeriodForDiningHall(dh)
-			if (mealPeriod) {
-				const startTime = moment(mealPeriod.startTime, timeFormat)
-				if (time.diff(startTime) > 0) {
-					continue
-				}
-			}
-		}
 		const mealPeriod = getCurrentMealPeriodForDiningHall(dh)
 		const dhFullname = convertDiningHall[dh.name]
 		if (mealPeriod) {
-			const startTime = moment(mealPeriod.startTime, timeFormat)
-			// INFO: consider finding a better way to determine if already sent
-			// one possible way to avoid missing / duplicated notifications
-			// is to call this larger intervals
-			if (Math.abs(moment().diff(startTime, 's')) < 60) {
-				// Opening notification
-				results.push({ title: `${dhFullname} is now open!`, body: `` })
-
-				// Notification for favorite meal
-				const favMeals = user.favMeals
-				if (favMeals) {
-					const meals = mealPeriod.subcategories.flatMap((subcategory) => {
-						return subcategory.meals
-					})
-					// INFO: consider merging into one notification
-					// factors to consider: too many meals
-					// that cannot be shown in the title / body
-					for (const meal of meals) {
-						if (favMeals.includes(meal)) {
-							results.push({
-								title: `${dhFullname} is now serving ${meal} !`,
-								body: ``
-							})
-						}
-					}
-				}
-			}
-		}
-		if (!dh.activityLevel) continue
-		if (dh.activityLevel <= thresholds[dh.name]) {
-			results.push({ title: `${dhFullname} activity is low now!`, body: `` })
-			await updateUserRecord(user, dhName)
+			results = results
+				.concat(await generateOpeningNotifications(
+					mealPeriod, user, dhFullname))
+				.concat(await generateActivityNotification(dh, user))
 		}
 	}
 	return results
 }
 
 async function updateUserRecord(user: TypeUser, dhName: DiningHallName) {
-	const schemaUser = await SchemeUser.findOne({ email: user.email })
+	const schemaUser = await SchemaUser.findOne({ email: user.email })
 	if (!schemaUser) return
 	let record = user.notificationsSent
 	record = record ? record : {
