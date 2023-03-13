5f7a3f2f04c36b457c55d66b4cab8fa111cf84f4
weitcis <weitcis@pm.me>
2023-03-13 04:48:30 -0700

    fix: gen-notifi logic
    - should map over all dining halls in the main gen-notifi function
    - move the check of favDiningHall existence

--- notification.ts
+++ notification.ts
@@ -32,22 +32,8 @@ export async function sendNotifications() {
 		SH: null,
 		DR: null,
 	}
-	const result: { [Property in DiningHallName]: number | null } = {
-		BP: null,
-		DN: null,
-		RE: null,
-		RW: null,
-		BC: null,
-		EC: null,
-		EA: null,
-		SH: null,
-		DR: null,
-	}
 	for (const dh of dhs) {
 		dhTypeMap[dh.name] = dh
-		if (dh.activityLevel) {
-			result[dh.name] = dh.activityLevel
-		}
 	}
 	for (const user of users) {
 		const tokens = user.notificationTokens
@@ -98,7 +84,9 @@ async function generateMealNotifications(
 	return results
 }
 
-async function generateActivityNotification(dh: TypeDiningHall, user: TypeUser): Promise<NotificationObject[]> {
+async function generateActivityNotification(
+	dh: TypeDiningHall,
+	user: TypeUser): Promise<NotificationObject[]> {
 	let results: NotificationObject[] = []
 	if (!dh.activityLevel) return []
 	if (checkAlreadySent(dh, user)) return []
@@ -110,20 +98,28 @@ async function generateActivityNotification(dh: TypeDiningHall, user: TypeUser):
 	return results
 }
 
-async function generateOpeningNotifications(mealPeriod: MealPeriod, user: TypeUser, dhFullname: string): Promise<NotificationObject[]> {
+async function generateOpeningNotifications(
+	mealPeriod: MealPeriod,
+	user: TypeUser,
+	dh: TypeDiningHall): Promise<NotificationObject[]> {
 	let results: NotificationObject[] = []
 	const startTime = moment(mealPeriod.startTime, timeFormat)
+	const dhFullname = convertDiningHall[dh.name]
 	// INFO: consider finding a better way to determine if already sent
 	// one possible way to avoid missing / duplicated notifications
 	// is to call this larger intervals
 	if (Math.abs(moment().diff(startTime, 's')) < 60) {
-		// Opening notification
-		results.push({ title: `${dhFullname} is now open!`, body: `` })
+		// Favorite dininghall opening notification
+		const favDiningHalls = user.favDiningHalls
+		if (favDiningHalls) {
+			if (favDiningHalls.includes(dh.name))
+				results.push({ title: `${dhFullname} is now open!`, body: `` })
+		}
 
 		// Notification for favorite meal
 		const favMeals = user.favMeals
 		if (favMeals) {
-			results = await generateMealNotifications(mealPeriod, favMeals, dhFullname)
+			results = results.concat(await generateMealNotifications(mealPeriod, favMeals, dhFullname))
 		}
 	}
 	return results
@@ -131,23 +127,21 @@ async function generateOpeningNotifications(mealPeriod: MealPeriod, user: TypeUs
 
 async function generateNotification(user: TypeUser,
 	dhs: { [Property in DiningHallName]: TypeDiningHall | null }): Promise<NotificationObject[]> {
-	if (!user.favDiningHalls) return []
 	const diningHallToNotificationsMap = async (dhName: DiningHallName) => {
 		const dh = dhs[dhName]
 		const empty: NotificationObject[] = []
 		if (!dh) return empty
 		const mealPeriod = getCurrentMealPeriodForDiningHall(dh)
-		const dhFullname = convertDiningHall[dh.name]
 		if (mealPeriod) {
 			const opening = await generateOpeningNotifications(
-				mealPeriod, user, dhFullname)
+				mealPeriod, user, dh)
 			const activity = await generateActivityNotification(dh, user)
 			return opening.concat(activity)
 		}
 		return empty
 	}
-	const unflattened = await Promise.all(user.favDiningHalls
-		.map(diningHallToNotificationsMap))
+	const dhNames = Object.keys(convertDiningHall) as DiningHallName[]
+	const unflattened = await Promise.all(dhNames.map(diningHallToNotificationsMap))
 	return unflattened.flat()
 }
 
